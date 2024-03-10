78e4bcee63d6f4eb60d6ae3b9e9a0d7aba83d200
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-03-10 17:05:47 +0530

    food ordering app initial setup

---
+++ temp/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+let heading = React.createElement("h1", {}, "Hello world from REACT from File");
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+// Lets create another div with id root2 and add multiple children of it using React.createElement
+
+// React.createElement return a react element which is basically a js object
+let heading2 = React.createElement(
+  "div",
+  {id: "root2"},
+  React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement(
+      "div",
+      {id: "child"},
+      [  
+        React.createElement(
+        "h2",
+        {},
+        "I am heading h2 from another div"
+      ), 
+        React.createElement(
+        "h3",
+        {},
+        "I am heading h3 sibling of h2"
+      )
+    ]
+    )
+  )
+)
+
+// JSX
+// Creating react element using core React package - React.createElement is very complex. That is why we use JSX to create react element
+
+let heading3 = <h3>This is heading 3</h3>
+// Heading 3 is a react element(js onject) which we can render on the DOM
+
+console.log("heading 3", heading3)
+
+
+// fuctional component 
+const HeaderComponent = () => {
+  return <h1>Hello world from functional component</h1>
+}
+
+// root.render basically convert this js object into html to get rendered into the DOM
+// there can only be one root
+root.render(heading3)
