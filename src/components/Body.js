4a5719567bc2e733d14e0d8951b433e9b8b8707f
Rishabh Tiwari <13rishabhtiwari@gmail.com>
2024-03-13 19:28:12 +0530

    header and body styling changed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,30 @@
 import { useEffect, useState } from 'react';
 import loopData from '../../utils/MOCK_DATA.json'
 import RestaurantCard from './RestaurantCard';
+import Loading from './Loading';
+import Shimmer from './Shimmer';
 
 const BodyContainer = () => {
-  let [restaurantsData, setRestaurantsData] = useState(loopData);
-  let [isChecked, setIsChecked] = useState(false)
-  
+  let [restaurantsData, setRestaurantsData] = useState([]);
+  let [isChecked, setIsChecked] = useState(false);
+  let [search, setSearch] = useState("");
+  let [loading, setLoading] = useState(true)
+
   useEffect(() => {
-    if(isChecked) {
-      let filterRestaurantData = restaurantsData.filter((res) => {
-        return res.info.avgRatingString > 4.5;
-      })
-      setRestaurantsData(filterRestaurantData)
-    } else {
-      setRestaurantsData(loopData)
-    }
-  }, [isChecked])
+    //make api call
+    fetchData().then((res) => setLoading(false))
+    
+  }, []);
+  
+
+  const fetchData = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        setRestaurantsData(loopData)
+        resolve();
+      }, 3000);
+    })
+  }
 
 
   const handleToggleCheckbox = () => {
@@ -23,12 +32,24 @@ const BodyContainer = () => {
 
   }
 
-  console.log(restaurantsData)
-  return(
+  const handleRestaurantSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
+  let filterData = restaurantsData.filter((res) => {
+    return (
+      (
+        (!isChecked || (isChecked && res.info.avgRating > 4.5)) && 
+        res.info.name.toLowerCase().includes(search)
+      )
+    )
+  })
+
+  return restaurantsData.length === 0 ? <Shimmer /> : (
     <div className='body-container'>
       <div className='action-container'>
         <div className='search-container'>
-          <input className="search" type='text' placeholder='Search Restaurants' />
+          <input className="search" type='text' value={search} placeholder='Search Restaurants' onChange={(e) => handleRestaurantSearch(e)}/>
         </div>
         <div className='filter-container'>
           <label htmlFor="restaurants">Top Rated Restaurants</label>
@@ -40,11 +61,12 @@ const BodyContainer = () => {
             onChange={handleToggleCheckbox}/>
         </div>
       </div>
-      <div className='res-container'>
-        {restaurantsData && restaurantsData.length > 0 && restaurantsData.map((res, index) => (
-          <RestaurantCard key={res.info.id} resData={res.info} />
-        ))}
-
+      <div className='parent-res-container'>
+        <div className='res-container'>
+          {filterData && filterData.length > 0 && filterData.map((res, index) => (
+            <RestaurantCard key={res.info.id} resData={res.info} />
+          ))}
+        </div>
       </div>
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { LOG_URL } from "../../utils/constant";
 
 const Header = () => {
+  const [loginBtn, setLoginBtn] = useState("Login")
+
+  const handleLogin = () => {
+    loginBtn === "Login" ? setLoginBtn("Logout") : setLoginBtn("Login")
+  }
   return (
     <div className='header'>
       <div className='logo-container'>
         <img className="logo" src={LOG_URL} />
+        <div className="img-color-overlay"></div>
       </div>
       <div className='nav-items'>
         <ul>
@@ -12,6 +19,9 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button className="login-btn" onClick={() => handleLogin()}>
+            {loginBtn}
+          </button>
         </ul>
       </div>
     </div>
